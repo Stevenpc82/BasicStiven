189039ae557dcdba803d8e64532d328f1cd34277
Stiven Pineda <jspineda74@misena.edu.co>
2021-08-08 16:23:44 -0500

    Update03

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.templates')
-@section('title','index')
+@section('title', 'index')
 @section('content')
 
-    <h1>Index</h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, amet assumenda distinctio dolores est expedita,
-        id, in libero molestiae nulla perferendis quaerat reprehenderit saepe sequi temporibus. Consequuntur ex quisquam
-        ratione.
-    </p>
+<h2>Index</h2>
+<div class="card text-center">
+    <div class="card-header">
+        Featured
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    <div class="card-footer text-muted">
+        2 days ago
+    </div>
+</div>
+<br>
+<Strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, in!</Strong>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellendus laudantium eligendi mollitia? Deserunt, ipsa
+    ipsam possimus minima veniam quaerat fugit cum totam! Pariatur possimus obcaecati culpa id distinctio praesentium
+    reiciendis a qui quis. Minus corrupti et quia tenetur voluptatibus dicta recusandae molestias illo, iste praesentium
+    dolorum amet qui id perferendis debitis. Amet, dolorem id asperiores consequatur accusamus harum sequi enim aperiam!
+    Labore explicabo, debitis, cupiditate optio blanditiis praesentium incidunt earum fuga ab ducimus quasi suscipit,
+    doloremque illum aspernatur perspiciatis placeat esse dolor porro aliquid! Odio libero, fuga non iste corporis
+    laborum veritatis! Soluta qui vel eos sequi doloremque adipisci?</p>
 @endsection
--- resources/views/layouts/templates.blade.php
+++ resources/views/layouts/templates.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,59 +8,96 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <title> page | @yield('title') </title>
 </head>
+
 <body>
 
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="inicio">Home</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="contactanos">Contactos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="acerca-de">Acerca de</a>
-              </li>
+            <a class="navbar-brand" href="inicio">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="contactanos">Contactos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="acerca-de">Acerca de</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <!--End Navbar -->
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <br>
+                <div class="d-grid gap-2 col-10 mx-auto">
+                    <button class="btn btn-secondary" type="button">Opcion 1</button>
+                    <button class="btn btn-secondary" type="button">Opcion 2</button>
+                    <button class="btn btn-secondary" type="button">Opcion 3</button>
+                    <button class="btn btn-secondary" type="button">Opcion 4</button>
+                    <button class="btn btn-secondary" type="button">Opcion 5</button>
+                    <button class="btn btn-secondary" type="button">Opcion 6</button>
+                  </div>
+                <br>
+            </div>
+            <div class="col-md-9">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+
+    <!--footer-->
+
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+            <ul class="list-unstyled text-small">
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Random feature</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team feature</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another one</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Last time</a></li>
             </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
+              <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
+            </ul>
           </div>
         </div>
-      </nav>
-
-      <!--End Navbar -->
-      <br>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-3">
-            <br>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-                <li class="list-group-item">A fourth item</li>
-                <li class="list-group-item">And a fifth one</li>
-              </ul>
-            <br>
-        </div>
-        <div class="col-md-9">
-            @yield('content')
-        </div>
-    </div>
-</div>
-
-<!--footer-->
-<footer class="bg-secondary">
-    <div class="container-fluid">
-        <p>&copy; Hola .inc</p>
-    </div>
-</footer>
+      </footer>
 
 </body>
+
 </html>
