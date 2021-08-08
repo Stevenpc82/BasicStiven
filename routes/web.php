fb768eb7213e588548f0bdef644d4759249bf5ec
Stiven Pineda <jspineda74@misena.edu.co>
2021-08-08 16:30:18 -0500

    Update4

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.templates');
+    return view('index');
 });
 Route::get('inicio', function () {
     return view('index');
